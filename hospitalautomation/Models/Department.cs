3fc6fe94fbb9920f5c8ed8cb0d533b90bac8498e
Melisa Simsek <melisaxsimsek@gmail.com>
2024-12-14 17:55:56 +0300

    nöbet ekleme işlemi

--- hospitalautomation/Models/Department.cs
+++ hospitalautomation/Models/Department.cs
@@ -1,15 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace hospitalautomation.Models
 {
-    public class Department:BaseEntity
+    public class Department : BaseEntity
     {
-        
-
         [Required]
         [StringLength(50)]
         public required string Name { get; set; }
@@ -18,9 +14,15 @@ namespace hospitalautomation.Models
         [StringLength(50)]
         public int PatientCount { get; set; }
 
-         [Required]
+        [Required]
         [StringLength(50)]
         public int EmptybedCount { get; set; }
 
+        [Required]
+        [DataType(DataType.Time)]
+        public TimeSpan StartTime { get; set; } 
+        [Required]
+        [DataType(DataType.Time)]
+        public TimeSpan EndTime { get; set; }
     }
 }
